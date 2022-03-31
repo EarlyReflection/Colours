f215a74193d736692fb2dafbae73bc56569fc961
Vladimir Dvornikov <echobizarre@gmail.com>
2022-03-31 21:18:26 +0300

    Initial Commit

---
+++ DvornikovVA_HW_2.2_Colours/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DvornikovVA_HW_2.2_Colours
+//
+//  Created by Vladimir Dvornikov on 31.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
