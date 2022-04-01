84cf68b38de02d3e9a857e6c9a4a80b90b6c54a1
Vladimir Dvornikov <echobizarre@gmail.com>
2022-04-01 17:03:59 +0300

    Basic Setup

--- DvornikovVA_HW_2.2_Colours/ViewController.swift
+++ DvornikovVA_HW_2.2_Colours/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colourView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+        
+        colourView.layer.cornerRadius = 10
+        
+        
+            
+        }
 }
+
+
+
 
