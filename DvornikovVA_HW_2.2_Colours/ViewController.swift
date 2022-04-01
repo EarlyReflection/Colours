7cbf53fdbcdf40972c1f5db927e3bfe911e23752
Vladimir Dvornikov <echobizarre@gmail.com>
2022-04-01 21:30:52 +0300

    Final

--- DvornikovVA_HW_2.2_Colours/ViewController.swift
+++ DvornikovVA_HW_2.2_Colours/ViewController.swift
@@ -8,17 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var colourView: UIView!
     
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colourView.layer.cornerRadius = 10
         
-        
-            
-        }
+        colourView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    
+    @IBAction func sliderAddingRed() {
+        redValueLabel.text = String(format:"%0.2f", redSlider.value)
+        colourView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    @IBAction func sliderAddingGreen() {
+        greenValueLabel.text = String(format:"%0.2f", greenSlider.value)
+        colourView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    @IBAction func sliderAddingBlue() {
+        blueValueLabel.text = String(format:"%0.2f", blueSlider.value)
+        colourView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
 }
 
 
